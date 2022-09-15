9a003b01018e37b3854f27558047c9011a2f85f2
Ivo Wermelinger <wermelinger@hi-schweiz.ch>
2022-09-15 10:59:04 +0200

    Updated the configuration and the service worker

--- frontend/svelte.config.js
+++ frontend/svelte.config.js
@@ -1,24 +1,25 @@
 import preprocess from 'svelte-preprocess';
 import adapter from '@sveltejs/adapter-node';
 
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-    preprocess: preprocess({
-        scss: {
-            prependData: `@import './src/style/app.scss';`
-        },
-    }),
-    kit: {
-        adapter: adapter(),
-
-        // Override http methods in the Todo forms
-        methodOverride: {
-            allowed: ['PATCH', 'DELETE']
-        },
-        env: {
-            publicPrefix: 'PUBLIC_',
-        }
+  preprocess: preprocess({
+    scss: {
+      prependData: `@import './src/style/app.scss';`
     },
+  }),
+  kit: {
+    adapter: adapter(),
+
+    // Override http methods in the Todo forms
+    methodOverride: {
+      allowed: ['PATCH', 'DELETE']
+    },
+    env: {
+      publicPrefix: 'PUBLIC_',
+    }
+  },
 };
 
 export default config;
