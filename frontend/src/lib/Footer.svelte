3ee5183f4236992d558186d716bf271b145a5d1d
Ivo Wermelinger <wermelinger@hi-schweiz.ch>
2022-09-30 16:27:47 +0200

    Added some backend configs

--- frontend/src/lib/Footer.svelte
+++ frontend/src/lib/Footer.svelte
@@ -1,17 +1,25 @@
 <script>
-    import {PUBLIC_STRAPI_API_URL} from '$env/static/public';
+  import {PUBLIC_STRAPI_API_URL} from '$env/static/public';
 </script>
 
 <footer id="footer" class="footer">
-
-    <div class="container">
-        <div class="row">
-            <div class="gr-12">
-                Footer
-            </div>
-            <div class="gr-12">
-                <a href={PUBLIC_STRAPI_API_URL + '/admin'} target="_blank" rel="noopener">Admin</a>
-            </div>
+  <div class="footer__background">
+    <div></div>
+    <div></div>
+  </div>
+  <div class="container">
+    <div class="row">
+      <div class="gr-20">
+        <div class="footer__content">
+          <div class="footer__phone"></div>
         </div>
+      </div>
+
+      <div class="gr-24 gr-4@small">
+        <div class="footer__black">
+          <a href={PUBLIC_STRAPI_API_URL + '/admin'} target="_blank" rel="noopener">Admin</a>
+        </div>
+      </div>
     </div>
+  </div>
 </footer>
--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -34,5 +34,4 @@
     </div>
   </div>
 
-  <ProjectGrid data="{data}"/>
 {/if}
